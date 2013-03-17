8d298cb0b0ba9747e784fcd483c80eaba2813ac3
Ajmal Afif <ajmalafif@gmail.com>
2013-03-17 18:29:28 +0800

    first commit to default Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazaar::Application.config.secret_token = 'cba4cdb8ffbc88a16bcebe5fc98c42ff969d058c5954cca46f68d8118e20d82e143adf75cbc68ca21732a1be7fd62210470b1d089a826037b13d1fb71620cdcc'
